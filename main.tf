4fad87a65954964964bf41c07bd55f08fcfbb3a0
erlend <erlenrom@stud.ntnu.no>
2024-10-20 18:19:34 +0200

    feat: set source-address-prefix to be within vnet-address-space

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "nsg" {
       protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "443"
-      source_address_prefix      = "*"
+      source_address_prefix      = "10.0.0.0/24"
       destination_address_prefix = "*"
     },
   }
