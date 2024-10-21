c8271fc1b025bdaf2a2ebbfce128cf4ae78f04cd
erlend <erlenrom@stud.ntnu.no>
2024-10-21 10:48:58 +0200

    feat: count instead of for_each in network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -30,8 +30,8 @@ resource "azurerm_network_security_group" "main" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "main" {
-  for_each = azurerm_subnet.main
+  count = length(azurerm_subnet.main)
 
-  subnet_id                 = each.value.id
+  subnet_id                 = azurerm_subnet.main[count.index].id
   network_security_group_id = azurerm_network_security_group.main.id
 }
