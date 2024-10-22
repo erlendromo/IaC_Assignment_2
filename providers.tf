68bd6b4decf5bf779a3edbbabe745a91c8598686
erlend <erlenrom@stud.ntnu.no>
2024-10-22 13:53:03 +0200

    feat: Minor changes

--- providers.tf
+++ providers.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   subscription_id = local.subscription_id
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
+  features {}
 }
 
