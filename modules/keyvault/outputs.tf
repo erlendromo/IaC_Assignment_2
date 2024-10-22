d5fba05ab78fb445c6bcaec7d846612af6311e09
erlend <erlenrom@stud.ntnu.no>
2024-10-22 13:01:37 +0200

    feat: Removed KV module

--- modules/keyvault/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "key_vault_id" {
-  value       = azurerm_key_vault.main.id
-  description = "The ID of the Key Vault."
-}
-
-output "key_vault_key_name" {
-  value       = azurerm_key_vault_key.main.name
-  description = "The name of the Key Vault Key."
-}
