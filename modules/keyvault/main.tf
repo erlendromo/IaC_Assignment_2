8009c8878df29d6b5490b015245d1de2d6a19af1
erlend <erlenrom@stud.ntnu.no>
2024-10-23 15:02:34 +0200

    feat: Setup module for keyvault with key for database

---
+++ modules/keyvault/main.tf
@@ -0,0 +1,39 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name = var.key_vault_name
+  resource_group_name = var.resource_group_name
+  location = var.resource_group_location
+  tenant_id = var.user_assigned_identity_tenant_id
+  enabled_for_disk_encryption = var.enabled_for_disk_encryption
+  public_network_access_enabled = var.public_network_access_enabled
+  sku_name = var.sku_name
+  soft_delete_retention_days = var.soft_delete_retention_days
+  purge_protection_enabled = var.purge_protection_enabled
+}
+
+resource "azurerm_key_vault_access_policy" "client" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  key_permissions = ["Get", "List", "Create", "Delete", "Update", "Recover", "Purge", "GetRotationPolicy"]
+}
+
+resource "azurerm_key_vault_access_policy" "user_assigned" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id = var.user_assigned_identity_tenant_id
+  object_id = var.user_assigned_identity_principal_id
+
+  key_permissions = ["Get", "WrapKey", "UnwrapKey"]
+}
+
+resource "azurerm_key_vault_key" "main" {
+  count = length(var.key_vault_keys)
+
+  key_vault_id = azurerm_key_vault.main.id
+  name = var.key_vault_keys[count.index].name
+  key_type = var.key_vault_keys[count.index].key_type
+  key_size = var.key_vault_keys[count.index].key_size
+  key_opts = var.key_vault_keys[count.index].key_opts
+}
---
+++ modules/keyvault/outputs.tf
@@ -0,0 +1,14 @@
+output "key_vault_id" {
+  value = azurerm_key_vault.main.id
+  description = "The ID of the key vault."
+}
+
+output "key_vault_name" {
+  value = azurerm_key_vault.main.name
+  description = "The name of the key vault."
+}
+
+output "key_vault_key_ids" {
+  value = tolist([for key in azurerm_key_vault_key.main : key.id])
+  description = "The IDs of the key vault keys."
+}
