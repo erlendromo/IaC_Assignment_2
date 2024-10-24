da50340efcb09a0b9157f7203807fdcf8de4d8ce
erlend <erlenrom@stud.ntnu.no>
2024-10-24 11:27:21 +0200

    feat: Storage module added

---
+++ modules/storage/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_storage_account" "main" {
+  name = var.storage_account_name
+  resource_group_name = var.resource_group_name
+  location = var.resource_group_location
+  account_tier = var.account_tier
+  account_kind = var.account_kind
+  account_replication_type = var.account_replication_type
+  https_traffic_only_enabled = var.https_traffic_only_enabled
+
+  network_rules {
+    default_action = "Deny"
+    virtual_network_subnet_ids = var.virtual_network_subnet_ids
+  }
+}
---
+++ modules/storage/outputs.tf
@@ -0,0 +1,9 @@
+output "storage_account_blob_endpoint" {
+  value = azurerm_storage_account.main.primary_blob_endpoint
+  description = "The endpoint URL for blob storage in the storage account."
+}
+
+output "storage_account_access_key" {
+  value = azurerm_storage_account.main.primary_access_key
+  description = "The primary access key for the storage account."
+}
