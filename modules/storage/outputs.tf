015022b30891db614f41be9b66c77e11435b1421
erlend <erlenrom@stud.ntnu.no>
2024-10-24 11:53:09 +0200

    feat: output SA-name

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -7,3 +7,8 @@ output "storage_account_access_key" {
   value       = azurerm_storage_account.main.primary_access_key
   description = "The primary access key for the storage account."
 }
+
+output "storage_account_name" {
+  value       = azurerm_storage_account.main.name
+  description = "The name of the storage account."
+}
