3dc0d3e6fb51938d523523ecd9d02d5d700a65d9
erlend <erlenrom@stud.ntnu.no>
2024-10-24 11:34:18 +0200

    feat: Split network rules for storage account into own resources

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -6,9 +6,21 @@ resource "azurerm_storage_account" "main" {
   account_kind               = var.account_kind
   account_replication_type   = var.account_replication_type
   https_traffic_only_enabled = var.https_traffic_only_enabled
+  min_tls_version = var.min_tls_version
+}
 
-  network_rules {
-    default_action             = "Deny"
-    virtual_network_subnet_ids = var.virtual_network_subnet_ids
-  }
+resource "azurerm_storage_account_network_rules" "default" {
+  storage_account_id = azurerm_storage_account.main.id
+  default_action = "Deny"
+  ip_rules = ["100.0.0.1"]
+  virtual_network_subnet_ids = var.virtual_network_subnet_ids
+  bypass = ["Metrics", "AzureServices"]
+}
+
+resource "azurerm_storage_account_network_rules" "localhost" {
+  storage_account_id = azurerm_storage_account.main.id
+  default_action = "Allow"
+  ip_rules = ["127.0.0.1"]
+  virtual_network_subnet_ids = var.virtual_network_subnet_ids
+  bypass = ["Metrics", "AzureServices"]
 }
