d2fe8e9b068b4306543da3a7e7868c863d4c8cc4
erlend <erlenrom@stud.ntnu.no>
2024-10-30 13:12:03 +0100

    feat: Modified backend-infrastructure entrypoint

--- backend_infrastructure/main.tf
+++ backend_infrastructure/main.tf
@@ -2,36 +2,8 @@ module "storage" {
   source                     = "../modules/storage"
   resource_group_name        = var.resource_group_name
   resource_group_location    = var.resource_group_location
-  virtual_network_subnet_ids = var.subnet_ids
 
   storage_account_name = "${var.base_prefix}sa${var.random_string}${var.workspace_suffix}"
-}
-
-module "key_vault" {
-  source                              = "../modules/keyvault"
-  resource_group_name                 = var.resource_group_name
-  resource_group_location             = var.resource_group_location
-  user_assigned_identity_tenant_id    = var.user_assigned_tenant_id
-  user_assigned_identity_principal_id = var.user_assigned_principal_id
-  subnet_id                           = var.subnet_ids[0]
-
-  key_vault_name = "${var.base_prefix}-kv-${var.workspace_suffix}"
-  key_vault_keys = [
-    {
-      name            = "storage-key"
-      key_type        = "RSA-HSM"
-      key_size        = 2048
-      key_opts        = ["unwrapKey", "wrapKey"]
-      expiration_date = "2024-12-31T23:59:00Z"
-    },
-    {
-      name            = "sql-key"
-      key_type        = "RSA-HSM"
-      key_size        = 2048
-      key_opts        = ["unwrapKey", "wrapKey"]
-      expiration_date = "2024-12-31T23:59:00Z"
-    }
-  ]
 }
 
 module "sql_database" {
@@ -40,15 +12,9 @@ module "sql_database" {
   resource_group_location             = var.resource_group_location
   administrator_login                 = var.random_string
   administrator_login_password        = var.random_password
-  user_assigned_identity_principal_id = var.user_assigned_principal_id
-  subnet_id                           = var.subnet_ids[0]
 
   server_name   = "${var.base_prefix}-sqlserver-${var.workspace_suffix}"
   database_name = "${var.base_prefix}-db-${var.workspace_suffix}"
-
-  storage_endpoint           = module.storage.storage_account_blob_endpoint
-  storage_account_access_key = module.storage.storage_account_access_key
-  key_vault_key_id           = module.key_vault.key_vault_key_ids[1]
 
   depends_on = [
     module.storage,
@@ -56,13 +22,16 @@ module "sql_database" {
   ]
 }
 
-resource "azurerm_storage_account_customer_managed_key" "main" {
-  storage_account_id = module.storage.storage_account_id
-  key_vault_id       = module.key_vault.key_vault_id
-  key_name           = module.key_vault.key_names[0]
+module "app_service" {
+  source                     = "../modules/app_service"
+  resource_group_name        = var.resource_group_name
+  resource_group_location    = var.resource_group_location
+  service_plan_name          = "${var.base_prefix}-sp-${var.workspace_suffix}"
+  linux_web_app_name         = "${var.base_prefix}-webapp-${var.workspace_suffix}"
+  storage_account_name       = module.storage.storage_account_name
+  storage_account_access_key = module.storage.storage_account_access_key
 
   depends_on = [
-    module.storage,
-    module.key_vault
+    module.storage
   ]
 }
