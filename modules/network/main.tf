8af8c75d7aa90dcdfb44db3f6063aa219a014398
erlend <erlenrom@stud.ntnu.no>
2024-11-06 15:33:11 +0100

    feat: Modified subnet-nsg association

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -56,7 +56,7 @@ resource "azurerm_network_security_rule" "main" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "main" {
-  subnet_id                 = azurerm_subnet.main.*.id
+  subnet_id                 = azurerm_subnet.main.id
   network_security_group_id = azurerm_network_security_group.main.id
 
   depends_on = [
