95dfe5cef0e4566a00f97931c2e23ba3ff3f5ec3
erlend <erlenrom@stud.ntnu.no>
2024-11-09 16:49:49 +0100

    feat: Modified fqdns for backend-address-pool for the web-app-slot

--- modules/app_service/outputs.tf
+++ modules/app_service/outputs.tf
@@ -2,5 +2,3 @@ output "webapp_public_ip" {
   value       = azurerm_public_ip.main.ip_address
   description = "The public IP address of the web app"
 }
-
-// TODO change this to gateway outbound IP
